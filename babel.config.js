0d278fcbd3a45716e41ff3e7fecf14d1b7c27019
Ciro Nunes <ciro@krint.io>
2019-03-18 10:23:37 +1100

    fix: export default

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,7 @@ module.exports = {
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime',
+    'add-module-exports',
   ],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: true
-        }
-      }
-    ], '@babel/preset-flow'],
+  presets: ['@babel/preset-env', '@babel/preset-flow'],
 };
--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,9 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.FlowWebpackPlugin = void 0;
+exports.default = void 0;
 
 var os = _interopRequireWildcard(require("os"));
 
@@ -17,167 +13,195 @@ var _process = _interopRequireDefault(require("process"));
 
 var _utils = require("./utils");
 
-const {
-  EOL
-} = os;
-const PLUGIN_PREFIX = '[flow-webpack-plugin]';
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-class FlowWebpackPlugin {
-  constructor(options) {
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var EOL = os.EOL;
+var PLUGIN_PREFIX = '[flow-webpack-plugin]';
+
+var FlowWebpackPlugin =
+/*#__PURE__*/
+function () {
+  function FlowWebpackPlugin(options) {
+    var _this = this;
+
+    _classCallCheck(this, FlowWebpackPlugin);
+
     options = options || {};
     this.options = (0, _utils.applyOptionsDefaults)(options);
     (0, _utils.validateOptions)(this.options);
 
     if (this.options.verbose) {
       (0, _utils.pluginPrint)('Options:');
-      Object.keys(this.options).forEach(optionName => (0, _utils.pluginPrint)(`${optionName}=${this.options[optionName]}`));
+      Object.keys(this.options).forEach(function (optionName) {
+        return (0, _utils.pluginPrint)("".concat(optionName, "=").concat(_this.options[optionName]));
+      });
     }
   }
 
-  apply(compiler) {
-    const plugin = this;
-    let flowResult;
-    let flowExecutionError = undefined;
+  _createClass(FlowWebpackPlugin, [{
+    key: "apply",
+    value: function apply(compiler) {
+      var plugin = this;
+      var flowResult;
+      var flowExecutionError = undefined;
 
-    const runCallback = failOnError => (compiler, webpackCallback) => {
-      flowCheck().then(result => {
-        flowResult = result;
-        callUserCallback(webpackCallback);
-      }).catch(error => {
-        flowExecutionError = error;
-        failOnError ? webpackCallback('Flow execution failed. ' + error) : webpackCallback();
+      var runCallback = function runCallback(failOnError) {
+        return function (compiler, webpackCallback) {
+          flowCheck().then(function (result) {
+            flowResult = result;
+            callUserCallback(webpackCallback);
+          }).catch(function (error) {
+            flowExecutionError = error;
+            failOnError ? webpackCallback('Flow execution failed. ' + error) : webpackCallback();
+          });
+        };
+      };
+
+      compiler.hooks.afterEmit.tap('FlowWebpackPlugin', function (compilation) {
+        var reportingCollectionName = _utils.REPORTING_SEVERITY[plugin.options.reportingSeverity];
+
+        if (flowExecutionError) {
+          /*
+           * Passed object will be printed at the end of the compilation in red
+           * (unless specified otherwise), webpack still emits assets,
+           * return code will still 0.
+           */
+          compilation[reportingCollectionName].push('Flow execution: ' + flowExecutionError); // callback()
+
+          return;
+        }
+
+        if (flowResult.successful) {
+          // callback()
+          return;
+        }
+
+        var details = plugin.options.printFlowOutput ? EOL + formatFlowOutput(flowResult) : '';
+        compilation[reportingCollectionName].push('Flow validation' + details); // callback()
       });
-    };
+      /*
+       * callbacks chosen because it is required
+       * * to be done before or at time of 'compilation callback' - to avoid expensive compilation when type error present
+       * * hook needs to be asynchronous
+       */
 
-    compiler.hooks.afterEmit.tap('FlowWebpackPlugin', compilation => {
-      const reportingCollectionName = _utils.REPORTING_SEVERITY[plugin.options.reportingSeverity];
+      compiler.hooks.run.tapAsync('FlowWebpackPlugin', runCallback(plugin.options.failOnError));
+      compiler.hooks.watchRun.tapAsync('FlowWebpackPlugin', runCallback(plugin.options.failOnErrorWatch));
 
-      if (flowExecutionError) {
-        /*
-         * Passed object will be printed at the end of the compilation in red
-         * (unless specified otherwise), webpack still emits assets,
-         * return code will still 0.
-         */
-        compilation[reportingCollectionName].push('Flow execution: ' + flowExecutionError); // callback()
+      function callUserCallback(webpackCallback) {
+        var userCallbackResult;
 
-        return;
-      }
+        try {
+          log('About to call user callback.');
+          userCallbackResult = plugin.options.callback(flowResult);
+        } catch (userCallbackException) {
+          console.warn(PLUGIN_PREFIX, 'User callback failed throwing:', userCallbackException);
+          afterUserCallback(webpackCallback);
+          return;
+        }
 
-      if (flowResult.successful) {
-        // callback()
-        return;
-      }
+        if (!(userCallbackResult instanceof Promise)) {
+          log('User callback synchronously ended.');
+          afterUserCallback(webpackCallback);
+          return;
+        }
 
-      const details = plugin.options.printFlowOutput ? EOL + formatFlowOutput(flowResult) : '';
-      compilation[reportingCollectionName].push('Flow validation' + details); // callback()
-    });
-    /*
-     * callbacks chosen because it is required
-     * * to be done before or at time of 'compilation callback' - to avoid expensive compilation when type error present
-     * * hook needs to be asynchronous
-     */
-
-    compiler.hooks.run.tapAsync('FlowWebpackPlugin', runCallback(plugin.options.failOnError));
-    compiler.hooks.watchRun.tapAsync('FlowWebpackPlugin', runCallback(plugin.options.failOnErrorWatch));
-
-    function callUserCallback(webpackCallback) {
-      let userCallbackResult;
-
-      try {
-        log('About to call user callback.');
-        userCallbackResult = plugin.options.callback(flowResult);
-      } catch (userCallbackException) {
-        console.warn(PLUGIN_PREFIX, 'User callback failed throwing:', userCallbackException);
-        afterUserCallback(webpackCallback);
-        return;
-      }
-
-      if (!(userCallbackResult instanceof Promise)) {
-        log('User callback synchronously ended.');
-        afterUserCallback(webpackCallback);
-        return;
-      }
-
-      log('User callback returned a promise. Waiting for it.');
-      userCallbackResult.then(() => {
-        log('User callback promise resolved.');
-        afterUserCallback(webpackCallback);
-      }, userCallbackException => {
-        console.warn(PLUGIN_PREFIX, 'Callback failed throwing:', userCallbackException);
-        afterUserCallback(webpackCallback);
-      });
-    }
-
-    function afterUserCallback(webpackCallback) {
-      if (!flowResult.successful && plugin.options.failOnError) {
-        const details = plugin.options.printFlowOutput ? EOL + formatFlowOutput(flowResult) : '';
-        /*
-         * argument passed to callback() causes webpack to immediately stop, even in watch mode,
-         * don't emit assets, and set return code to 1
-         */
-
-        webpackCallback('Flow validation failed.' + details);
-        return;
-      }
-
-      webpackCallback();
-    }
-
-    function formatFlowOutput(result) {
-      return prefixIfVerbose('flow stdout', result.stdout) + prefixIfVerbose('flow stderr', result.stderr);
-    }
-
-    function prefixIfVerbose(prefix, lines) {
-      return plugin.options.verbose ? (0, _utils.prefixLines)(prefix, lines) : lines;
-    }
-
-    function flowCheck() {
-      return new Promise((resolve, reject) => {
-        log(`spawning flow`);
-        const flowProcess = (0, _child_process.spawn)(plugin.options.flowPath, plugin.options.flowArgs, {
-          stdio: getStdioOptions()
+        log('User callback returned a promise. Waiting for it.');
+        userCallbackResult.then(function () {
+          log('User callback promise resolved.');
+          afterUserCallback(webpackCallback);
+        }, function (userCallbackException) {
+          console.warn(PLUGIN_PREFIX, 'Callback failed throwing:', userCallbackException);
+          afterUserCallback(webpackCallback);
         });
-        let stdout = '';
-        let stderr = '';
-        plugin.options.printFlowOutput && flowProcess.stdout.on('data', data => stdout += data.toString());
-        plugin.options.printFlowOutput && flowProcess.stderr.on('data', data => stderr += data.toString());
-        let resolved = false;
-        flowProcess.on('error', error => {
-          (0, _utils.pluginPrint)('flow execution failed. Please make sure that the `flowPath` option is correctly set.', error);
-          reject(error);
-          resolved = true;
-        });
-        flowProcess.on('exit', exitCode => {
-          log('flow exited with return code ' + exitCode);
+      }
 
-          if (resolved) {
-            return;
-          }
+      function afterUserCallback(webpackCallback) {
+        if (!flowResult.successful && plugin.options.failOnError) {
+          var details = plugin.options.printFlowOutput ? EOL + formatFlowOutput(flowResult) : '';
+          /*
+           * argument passed to callback() causes webpack to immediately stop, even in watch mode,
+           * don't emit assets, and set return code to 1
+           */
 
-          resolve({
-            get successful() {
-              return this.exitCode === 0;
-            },
+          webpackCallback('Flow validation failed.' + details);
+          return;
+        }
 
-            exitCode,
-            stdout,
-            stderr
+        webpackCallback();
+      }
+
+      function formatFlowOutput(result) {
+        return prefixIfVerbose('flow stdout', result.stdout) + prefixIfVerbose('flow stderr', result.stderr);
+      }
+
+      function prefixIfVerbose(prefix, lines) {
+        return plugin.options.verbose ? (0, _utils.prefixLines)(prefix, lines) : lines;
+      }
+
+      function flowCheck() {
+        return new Promise(function (resolve, reject) {
+          log("spawning flow");
+          var flowProcess = (0, _child_process.spawn)(plugin.options.flowPath, plugin.options.flowArgs, {
+            stdio: getStdioOptions()
+          });
+          var stdout = '';
+          var stderr = '';
+          plugin.options.printFlowOutput && flowProcess.stdout.on('data', function (data) {
+            return stdout += data.toString();
+          });
+          plugin.options.printFlowOutput && flowProcess.stderr.on('data', function (data) {
+            return stderr += data.toString();
+          });
+          var resolved = false;
+          flowProcess.on('error', function (error) {
+            (0, _utils.pluginPrint)('flow execution failed. Please make sure that the `flowPath` option is correctly set.', error);
+            reject(error);
+            resolved = true;
+          });
+          flowProcess.on('exit', function (exitCode) {
+            log('flow exited with return code ' + exitCode);
+
+            if (resolved) {
+              return;
+            }
+
+            resolve({
+              get successful() {
+                return this.exitCode === 0;
+              },
+
+              exitCode: exitCode,
+              stdout: stdout,
+              stderr: stderr
+            });
           });
         });
-      });
-    }
+      }
 
-    function getStdioOptions() {
-      return plugin.options.printFlowOutput || plugin.options.callback === _utils.NOOP ? 'pipe' : 'ignore';
-    }
+      function getStdioOptions() {
+        return plugin.options.printFlowOutput || plugin.options.callback === _utils.NOOP ? 'pipe' : 'ignore';
+      }
 
-    function log(...messages) {
-      if (plugin.options.verbose) {
-        (0, _utils.pluginPrint)(...messages);
+      function log() {
+        if (plugin.options.verbose) {
+          _utils.pluginPrint.apply(void 0, arguments);
+        }
       }
     }
-  }
+  }]);
 
-}
+  return FlowWebpackPlugin;
+}();
 
-exports.FlowWebpackPlugin = FlowWebpackPlugin;
+var _default = FlowWebpackPlugin;
+exports.default = _default;
+module.exports = exports.default;
